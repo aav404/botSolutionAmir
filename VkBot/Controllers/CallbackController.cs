6ecbc16f935fd97e9a71625bc18381422a761a61
Vyatkin A.A <sanyavjatkin@gmail.com>
2021-08-06 01:19:41 +0300

    не протестированная бетта, которая требует доработки

--- VkBot/Controllers/CallbackController.cs
+++ VkBot/Controllers/CallbackController.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using VkBot.Dtos;
 using VkNet.Abstractions;
 using VkNet.Model;
+using VkNet.Model.Keyboard;
 using VkNet.Model.RequestParams;
 using VkNet.Utils;
 
@@ -26,11 +28,30 @@ namespace VkBot.Controllers
         /// </summary>
         private readonly IVkApi _vkApi;
 
+        private readonly Dictionary<string, string> questions = new Dictionary<string, string>
+        {
+            {
+                "button1", "За ваши модификации дают бан?"
+            },
+            {
+                "button2", "Как начать пользоваться D2MO?"
+            },
+            {
+                "button3", "Как купить работу?"
+            },
+            {
+                "adminConnectButton", "Связаться с администратором."
+            },
+        };
+
+        private List<long> communicatingWithAdmin = new List<long>();
+
         public CallbackController(IVkApi vkApi, IConfiguration configuration, IMapper mapper)
         {
             _vkApi = vkApi;
             _configuration = configuration;
             _mapper = mapper;
+
         }
 
         [HttpPost]
@@ -48,18 +69,20 @@ namespace VkBot.Controllers
                 // Новое сообщение
                 case "message_new":
                     {
-                        var keyboard = new VkNet.Model.Keyboard.KeyboardBuilder("text"); 
-                        keyboard.AddButton("Тестовая кнопка", "test_1"); 
-                        keyboard.SetInline(false);
-
-                        _vkApi.Messages.Send(new MessagesSendParams
+                        if (updates.Object.Body.ToLower() == "начать")
                         {
-                            UserId = updates.Object.User_id,
-                            RandomId = Extensions.GenerateRandomId(),
-                            PeerId = updates.GroupId,
-                            Message = "тест бота",
-                            Keyboard = keyboard.Build(),
-                        });
+                            SendMenu(updates);
+
+                            communicatingWithAdmin.Remove(updates.Object.User_id);
+                        }
+                        else if (communicatingWithAdmin.Contains(updates.Object.User_id))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            SendResponse(updates);
+                        }
 
                         break;
                     }
@@ -68,10 +91,100 @@ namespace VkBot.Controllers
             return Ok("ok");
         }
 
-        [HttpGet]
-        public IActionResult GetLogs()
+        private void SendMenu(UpdatesDto updates)
         {
-            return Ok("Работает");
+            var keyboard = new VkNet.Model.Keyboard.KeyboardBuilder("text");
+            keyboard.SetInline(true);
+
+            foreach (var question in questions)
+            {
+                keyboard.AddButton(question.Value, question.Key);
+                keyboard.AddLine();
+            }
+
+            SendMessage(updates.Object.User_id, updates.GroupId, "Выбери вопрос", keyboard.Build());
+        }
+
+        private void SendResponse(UpdatesDto updates)
+        {
+            var keyboard = new VkNet.Model.Keyboard.KeyboardBuilder("text");
+            keyboard.SetInline(true);
+
+            switch (updates.Object.Body)
+            {
+                case "За ваши модификации дают бан?":
+                    {
+                        SendMessage(updates.Object.User_id, updates.GroupId, "Все моды сделаны посредством софта, которое предоставляют Valve, за это невозможно получить бан. Абсолютно по такому же принципу создатели сетов из Workshop подгружают свои работы в клиент игры. Мы не вмешиваемся в данные, которые отправляются на сервера Valve.");
+
+                        break;
+                    }
+                case "Как начать пользоваться D2MO?":
+                    {
+                        SendMessage(updates.Object.User_id, updates.GroupId, @"1) Вам необходимо скачать программу из статьи: (https://vk.com/@amir4anmods-taverna).
+  2) Следом перейти в обсуждения и установить по инструкций: (https://vk.com/topic-178507216_39493678).
+  3) Установка модов указана здесь: (https://vk.com/topic-178507216_39493678).
+  4) Решение всех проблем указано здесь: (https://vk.com/topic-178507216_39493681).");
+
+                        break;
+                    }
+                case "Как купить работу?":
+                    {
+                        SendMessage(updates.Object.User_id, updates.GroupId, @"1) Вы можете оформить подписку Donuts и получить доступ к эксклюзивной коллекций работ нашего производства. Вся информация касаемо платформы Donuts представлена в этой статье: (https://vk.com/@donut-faq-dlya-donov). Подписка оформляется по этой ссылке: (https://vk.com/amir4anmods?source=description&w=donut_payment-178507216).
+  2) Так же в товарах группы присутствуют работы, которые продаются поштучно.
+  3) Заказать эксклюзивную сборку можно, связавшись с администратором группы.");
+
+                        break;
+                    }
+                case "Связаться с администратором.":
+                    {
+                        communicatingWithAdmin.Add(updates.Object.User_id);
+                        PingAdmin(updates);
+                        SendMessage(updates.Object.User_id, updates.GroupId, "Админ ответит в скором времени :)");
+
+                        break;
+                    }
+                default:
+                    {
+                        SendMessage(updates.Object.User_id, updates.GroupId, "Нажми на одну из кнопок, чтобы получить ответ :)");
+
+                        break;
+                    }
+            }
+
+            //_vkApi.Messages.MarkAsImportantConversation(updates.GroupId, false);
+        }
+
+        private void PingAdmin(UpdatesDto updates)
+        {
+            _vkApi.Messages.Send(new MessagesSendParams
+            {
+                UserId = 322696326,
+                RandomId = Extensions.GenerateRandomId(),
+                PeerId = updates.GroupId,
+                Message = "Тебя пингует какой то долбаёб, чекни группу",
+                //Forward = new MessageForward()
+                //{
+                //    OwnerId = updates.Object.User_id,
+                //    PeerId = updates.GroupId,
+                //    ConversationMessageIds =
+                //    new long[]
+                //    { updates.Object.Id },
+                //    MessageIds = new long[]
+                //    { updates.Object.Id }
+                //}
+            });
+        }
+
+        private void SendMessage(long userId, long groupId, string message, MessageKeyboard keyboard = null)
+        {
+            _vkApi.Messages.Send(new MessagesSendParams
+            {
+                UserId = userId,
+                RandomId = Extensions.GenerateRandomId(),
+                PeerId = groupId,
+                Message = message,
+                Keyboard = keyboard,
+            });
         }
     }
 }
